635f60c511955c62cc7ad777b8036bd668b2fe93
seanoneill <seansaysmeh@gmail.com>
2017-09-01 19:07:46 +0700

    removing lodash dependency

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var _ = require("lodash");
 
 module.exports = function(port, middleware, callback) {
     var app = express();
